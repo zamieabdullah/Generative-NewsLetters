7f2ca6b74496b64620c073051aea31ea4c260a9b
zamieabdullah <zamieabdullah0@gmail.com>
2024-08-02 01:25:43 -0400

    debugging

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 import streamlit as st
 from generate import generate
+import os
 
+print("Current Working Directory:", os.getcwd())
 blog = None
 # Show a spinner while waiting for the blog to generate
 with st.spinner("Waiting for blog to load..."):
