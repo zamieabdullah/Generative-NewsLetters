e9ec126460edf2b254191b96cafe9fbe411f88ae
zamieabdullah <zamieabdullah0@gmail.com>
2024-09-05 00:09:39 -0400

    adding run.py for gunicorn

---
+++ models/run.py
@@ -0,0 +1,40 @@
+from flask import Flask
+from flask_migrate import Migrate
+from flask_cors import CORS
+from models.model import db
+import os
+
+# importing environment variables 
+from dotenv import load_dotenv
+load_dotenv()
+
+# importing models
+from models.blogposts import BlogPosts
+
+# importing routes
+from views.blogposts import bp as blogpost_routes
+
+# instantiate the app
+app = Flask(__name__)
+app.config.from_object(__name__)
+
+# Configure PostgreSQL connection
+app.config['SQLALCHEMY_DATABASE_URI'] = f"postgresql://{os.getenv('DB_USER')}:{os.getenv('DB_PASSWORD')}@" \
+                                        f"{os.getenv('DB_HOST')}:{os.getenv('DB_PORT')}/{os.getenv('DB_NAME')}"
+db.init_app(app)
+migrate = Migrate(app, db)
+app.config['SQLALCHEMY_ECHO'] = True
+
+# Create the database tables (if they don't exist)
+with app.app_context():
+    db.create_all()
+
+
+# Register the routes blueprint
+app.register_blueprint(blogpost_routes)
+
+# enable CORS
+CORS(app, resources={r'/*': {'origins': '*'}})
+
+if __name__ == '__main__':
+    app.run()
